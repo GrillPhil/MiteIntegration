5b0ed093bb644c232179f41f66a277cb878c3623
AzureAD\PhilippBauknecht <bauknecht@medialesson.de>
2018-05-31 22:33:16 +0200

    Initial commit

---
+++ ServerlessMiteIntegrationDemo/Project.cs
@@ -0,0 +1,18 @@
+﻿namespace ServerlessMiteIntegrationDemo
+{
+    class Project
+    {
+        public int Budget { get; set; }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int ConsumedBudget { get; set; }
+    }
+
+    class ProjectWrapper
+    {
+        public Project Project { get; set; }
+    }
+}
---
+++ ServerlessMiteIntegrationDemo/TimeEntry.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+namespace ServerlessMiteIntegrationDemo
+{
+    class TimeEntry
+    {
+        [JsonProperty("project_id")]
+        public int ProjectId { get; set; }
+
+        public int Minutes { get; set; }
+    }
+
+    class TimeEntryWrapper
+    {
+        [JsonProperty("time_entry")]
+        public TimeEntry TimeEntry { get; set; }
+    }
+}
